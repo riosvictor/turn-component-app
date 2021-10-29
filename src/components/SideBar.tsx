13f7ca1d6290ea99fef3e08fb24b66f196149059
Paulo Victor Rios <victorios.ba@gmail.com>
2021-10-29 11:47:32 -0300

    finish challenge

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,33 @@
-export function SideBar() {
-  // Complete aqui
+import { useState } from "react";
+import { GenreResponseProps } from "../App";
+import { Button } from "./Button";
+
+import '../styles/global.scss';
+import '../styles/sidebar.scss';
+
+interface IProps {
+  genres: GenreResponseProps[]
+  handleClickButton: (id: number) => void
+  selectedGenreId?: number
+}
+
+export function SideBar({ genres, handleClickButton, selectedGenreId = 1 }: IProps) {
+  return (
+    <nav className="sidebar">
+        <span>Watch<p>Me</p></span>
+
+        <div className="buttons-container">
+          {genres.map(genre => (
+            <Button
+              key={String(genre.id)}
+              title={genre.title}
+              iconName={genre.name}
+              onClick={() => handleClickButton(genre.id)}
+              selected={selectedGenreId === genre.id}
+            />
+          ))}
+        </div>
+
+      </nav>
+  )
 }
